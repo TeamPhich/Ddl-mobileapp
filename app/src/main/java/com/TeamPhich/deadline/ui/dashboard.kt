bacfb282fea774df01ed8c40f06370613a0f78ba
diepninh <ninhhongdiep@gmail.com>
2019-11-03 23:51:00 +0700

    group+space

---
+++ app/src/main/java/com/TeamPhich/deadline/ui/dashboard.kt
@@ -0,0 +1,18 @@
+package com.TeamPhich.deadline.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.TeamPhich.deadline.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activiy_showmenu.*
+
+class dashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_a_space)
+
+
+    }
+}
