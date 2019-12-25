ae30ecc1a15ebde982c3ef944d2232c6d2ca2787
diepninh <ninhhongdiep@gmail.com>
2019-12-25 13:23:46 +0700

    12

--- app/src/main/java/com/TeamPhich/deadline/ui/dashboard/dashboard.kt
+++ app/src/main/java/com/TeamPhich/deadline/ui/dashboard/dashboard.kt
@@ -66,8 +66,8 @@ class dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedLi
         avartauseringroup.setOnClickListener { view ->
             startActivity(intent)
         }
-        val tablayoutTask = Tablayout_task.newInstance()
-        openFragment(tablayoutTask)
+        val groupFragment = GroupFragment.newInstance()
+        openFragment(groupFragment)
 
 
     }
